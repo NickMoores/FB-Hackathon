b9721733af75f32b4da2062ce6a624d9d0189e6e
Nick Moores <nick@moores-home.co.uk>
2017-03-29 13:05:34 +0100

    Added unit of work.

---
+++ API/ITN.Felicity.EntityFramework/UnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITN.Felicity.EntityFramework
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext dbContext;
+
+        public UnitOfWork(DbContext dbContext)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            this.dbContext = dbContext;
+        }
+
+        public Task SaveChangesAsync() => this.dbContext.SaveChangesAsync();
+    }
+}
