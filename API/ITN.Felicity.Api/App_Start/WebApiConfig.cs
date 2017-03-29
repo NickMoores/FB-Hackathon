dc53b783a15dfffd7025f485449dafc0568e8034
Michael Freeman <mike.c.freeman@gmail.com>
2017-03-29 13:04:17 +0100

    Added Feedback API Controller

--- API/ITN.Felicity.Api/App_Start/WebApiConfig.cs
+++ API/ITN.Felicity.Api/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace ITN.Felicity.Api
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ API/ITN.Felicity.Api/Controllers/FeedbackController.cs
@@ -0,0 +1,31 @@
+﻿using ITN.Felicity.Api.Models;
+using ITN.Felicity.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ITN.Felicity.Api.Controllers
+{
+    public class FeedbackController : ApiController
+    {
+        private readonly IArticleRepository _repo;
+        public FeedbackController(IArticleRepository repo)
+        {
+            this._repo = repo;
+        }
+
+        // POST: api/Feedback
+        public async Task Post([FromBody] FeedbackModel fm)
+        {
+            var article = await _repo.FindByUrlAsync(fm.ArticleURL);
+            if (article == null)
+                article = new Domain.Article(Guid.NewGuid(), fm.ArticleURL);
+            article.AddFeedback(fm.Feedback);
+        }
+
+    }
+}
